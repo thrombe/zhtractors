cce4924dbd2d51b1c3cd5f1946594002a0dfdd60
thrombe <yash.n.thombare620@gmail.com>
2025-09-17 00:03:16 +0530

    bring back randomize code

--- src/shader.glsl
+++ src/shader.glsl
@@ -102,6 +102,20 @@ void set_seed(int id) {
         Particle p = particles[id];
         // ParticleType pt = particle_types[p.type_index];
 
+        // randomize particles
+        {
+            if (ubo.params.randomize_particle_types != 0) {
+                p.type_index = randuint() % ubo.params.particle_type_count;
+                p.age = 0.0;
+                p.exposure = 0.0;
+            }
+            if (ubo.params.randomize_particle_attrs != 0) {
+                vec3 world = vec3(float(ubo.params.world_size_x), float(ubo.params.world_size_y), float(ubo.params.world_size_z));
+                p.pos = vec3(random(), random(), random()) * world;
+                p.vel = (vec3(random(), random(), random()) - 0.5) * 2000;
+            }
+        }
+
         ivec3 bpos = ivec3(p.pos / ubo.params.bin_size);
         ivec3 bworld = ivec3(ubo.params.bin_buf_size_x, ubo.params.bin_buf_size_y, ubo.params.bin_buf_size_z);
 
